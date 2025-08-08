2e5ec7fd2b6320741823a977c3b69a13bde9752f
Anmol <anmol18125@gmail.com>
2025-08-08 19:28:42 +0530

    Fix budgetService path case sensitivity

--- server/routes/progress.js
+++ server/routes/progress.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const Progress = require('../models/Progress');
-const budgetService = require('../services/budgetService');
+const budgetService = require('../services/budgetServices');
 
 /**
  * GET /api/progress
